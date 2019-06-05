5aa5c1e50b040c13a016f81de6c4434a105ab1b3
Martin Hristov <martinhristov@Martins-MacBook-Pro.local>
2019-06-05 09:07:42 +0300

    Using terraform fmt

--- main.tf
+++ main.tf
@@ -1,31 +1,32 @@
 # Defining network to put the VM in
 module "resourceGroup" {
-  source = "./modules/resourceGroup"
-  RGname = "${var.RGname}"
+  source   = "./modules/resourceGroup"
+  RGname   = "${var.RGname}"
   location = "${var.location}"
-  
 }
+
 module "network" {
-    source = "./modules/network"
-    #Creates dependency, waits for resourceGroup to finish.
-    RGname = "${module.resourceGroup.RGname}"
-    location = "${var.location}"
+  source = "./modules/network"
+
+  #Creates dependency, waits for resourceGroup to finish.
+  RGname   = "${module.resourceGroup.RGname}"
+  location = "${var.location}"
 }
+
 # Defining the actual machine using module "compute" from Terraform Registry.
 module "linuxserver" {
-    source = "Azure/compute/azurerm"
-    location = "${var.location}"
-    vm_os_simple = "${var.os}"
-    public_ip_dns = ["linux1234"]
-    #Creates dependency, waits for resourceGroup to finish.
-    resource_group_name = "${module.resourceGroup.RGname}"
-    vnet_subnet_id = "${module.network.trainingNetworksSubnetId}"
-    storage_account_type = "Standard_LRS"
-    vm_size = "Standard_B1ls"
-    tags {
-      "environment" = "trainingENV"
-    }
+  source        = "Azure/compute/azurerm"
+  location      = "${var.location}"
+  vm_os_simple  = "${var.os}"
+  public_ip_dns = ["linux1234"]
+
+  #Creates dependency, waits for resourceGroup to finish.
+  resource_group_name  = "${module.resourceGroup.RGname}"
+  vnet_subnet_id       = "${module.network.trainingNetworksSubnetId}"
+  storage_account_type = "Standard_LRS"
+  vm_size              = "Standard_B1ls"
+
+  tags {
+    "environment" = "trainingENV"
+  }
 }
-
-
-
--- modules/network/network.tf
+++ modules/network/network.tf
@@ -3,6 +3,7 @@ resource "azurerm_virtual_network" "trainingNetwork" {
   address_space       = ["10.0.0.0/16"]
   location            = "${var.location}"
   resource_group_name = "${var.RGname}"
+
   # Tagging for easier search.
   tags {
     environment = "trainingENV"
